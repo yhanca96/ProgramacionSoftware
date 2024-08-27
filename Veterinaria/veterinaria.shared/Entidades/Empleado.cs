814580c7257a0a8e51dace6d3def434501fe800c
yhanca96 <yhann.423@hotmail.com>
2024-08-26 21:27:55 -0500

    creacion de empleado

---
+++ Veterinaria/veterinaria.shared/Entidades/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veterinaria.shared.Entidades
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+    }
+}
